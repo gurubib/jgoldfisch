9c490bb6f48e59ebbc23cdcffcc983daad79cff3
Barnabas Gurubi <gurubibarni@gmail.com>
2018-03-09 22:20:11 +0100

    Skeleton - BoxRecorder váz

---
+++ Workspace/Skeleton/src/skeleton/game/logic/BoxRecorder.java
@@ -0,0 +1,25 @@
+package skeleton.game.logic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BoxRecorder {
+	
+	private List<Field> fixFields = new ArrayList<Field>();
+	
+	public void init(List<Field> walls) {
+		fixFields = walls;
+	}
+	
+	public void checkRecordWith(Field f) {
+		//TODO
+	}
+	
+	public void endFieldOccupied(Field f) {
+		//TODO
+	}
+	
+	public void update(Field f) {
+		//TODO
+	}
+}
