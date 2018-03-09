c889d5d3cde9d16de02740c0d003ddf7b6a98fdf
Barnabas Gurubi <gurubibarni@gmail.com>
2018-03-10 00:29:43 +0100

    Skeleton - kérdezgetés kipróbálása

--- Workspace/Skeleton/src/skeleton/game/logic/SimpleField.java
+++ Workspace/Skeleton/src/skeleton/game/logic/SimpleField.java
@@ -1,5 +1,9 @@
 package skeleton.game.logic;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class SimpleField extends Field {
 
 	@Override
@@ -19,10 +23,33 @@ public class SimpleField extends Field {
 
 	@Override
 	public Movable workerEnters(Worker w, Direction d) {
-		System.out.println("SimpleField.workerEnters(w, d)");
+		System.out.println("~SimpleField.workerEnters(w, d)");
 		
 		w.place(this);
 		getNeighbor(d.getOpposite()).remove();
+		
+		System.out.println("What is on this field? (1): A Box; (2): Nothing");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = 0;
+		try {
+			n = Integer.parseInt(br.readLine());
+		} catch (NumberFormatException | IOException e) {
+			e.printStackTrace();
+		}
+		
+		switch (n) {
+			case 1 : 
+				Box b = new Box();
+				b.setField(this);
+				this.setMovable(b);
+				break;
+			case 2 :
+				this.setMovable(null);
+				break;
+			default:
+				this.setMovable(null);
+		}
 		
 		Movable movable = getMovable();
 		
