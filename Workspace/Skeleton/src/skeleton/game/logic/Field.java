c94fedb057c4ba7285b0d9912f8ea09ce93a9057
Markerer <36426659+Markerer@users.noreply.github.com>
2018-03-16 15:57:34 +0100

    Kommentek hozzáadva

--- Workspace/Skeleton/src/skeleton/game/logic/Field.java
+++ Workspace/Skeleton/src/skeleton/game/logic/Field.java
@@ -4,7 +4,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 import skeleton.out.MethodWriter;
-
+/**
+ * Az egyes mező típusok absztrakt ősosztálya.
+ */
 public abstract class Field {
 	
 	private String skeletonName;
@@ -20,28 +22,50 @@ public abstract class Field {
 	public String toString() {
 		return skeletonName;
 	}
+	/**
+	 * A név beállítására szolgáló függvény.
+	 * @param skeletonName A beállítandó név.
+	 */
 	public void setSkeletonName(String skeletonName) {
 		this.skeletonName = skeletonName;
 	}
+	/**
+	 * A mező paraméterben átadott irányban lévő szomszédjának getter függvénye.
+	 * @param d A kért irány.
+	 * @return A kért irányban lévő szomszéd.
+	 */
 	public Field getNeighbor(Direction d) {
 		MethodWriter.printOutMethod("Field.getNeighbor", "d");
 		
 		MethodWriter.printOutRet("f");
 		return neighbors.get(d);
 	}
-	
+	/**
+	 * A mező szomszédjának beállítása.
+	 * @param d A szomszéd iránya.
+	 * @param f	A szomszéd mező referenciája.
+	 */
 	public void setNeighbor(Direction d, Field f) {
 		neighbors.put(d, f);
 	}
-	
+	/**
+	 * A mezőn lévő Movable referencia beállítása.
+	 * @param m A beállítani kívánt Movable referencia.
+	 */
 	public void setMovable(Movable m) {
 		movable = m;
 	}
-	
+	/**
+	 * A mezőn tárolt Movable referencia visszaadására szolgáló függvény.
+	 * @return A mezőn lévő Movable referencia.
+	 */
 	public Movable getMovable() {
 		return movable;
 	}
-	
+	/**
+	 * A mezőn lévő Movable referencia kitörlésére szolgáló függvény.
+	 * @param m A kitörlendő Movable referencia.
+	 */
 	public void remove(Movable m) {
 		MethodWriter.printOutMethod("Field.remove", m.toString());
 		
@@ -50,16 +74,24 @@ public abstract class Field {
 		
 		MethodWriter.printOutRet("");
 	}
-	
+	/**
+	 * A Recorder osztály frissítésére szolgáló függvény.
+	 */
 	public void updateRecorder() {
 		//TODO
 	}
-	
+	/**
+	 * Függvény arra az esetre, ha egy doboz érkezik egy célmezőre.
+	 * @param b Az érkező doboz referenciája.
+	 */
 	public void boxArrived(Box b) {
 		MethodWriter.printOutMethod("EndField.boxArrived",b.toString());
 		MethodWriter.printOutRet("");
 	}
-	
+	/**
+	 * Függvény arra az esetre, ha egy munkás érkezik a célmezőre.
+	 * @param w Az érkező munkás referenciája.
+	 */
 	public void workerArrived(Worker w) {
 		MethodWriter.printOutMethod("EndField.workerArrived",w.toString());
 		MethodWriter.printOutRet("");
