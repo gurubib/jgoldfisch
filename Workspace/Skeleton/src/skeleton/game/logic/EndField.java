f28948659068f0db5c6c87af05cbdd93d03217a9
Akos11 <36598710+Akos11@users.noreply.github.com>
2018-03-18 23:10:14 +0100

    endfield, wallfield

--- Workspace/Skeleton/src/skeleton/game/logic/EndField.java
+++ Workspace/Skeleton/src/skeleton/game/logic/EndField.java
@@ -2,18 +2,36 @@ package skeleton.game.logic;
 
 import skeleton.out.MethodWriter;
 
+/**Célmező, amire a dobozokat (Box) juttatni kell. 
+ * Amennyiben rálép egy doboz, akkor az mozdíthatatlanná válik ott.
+ *A Field absztrakt ősosztály leszármazottja.
+ * 
+ * @author jgoldfisch
+ *
+ */
+*
+*
+*/
 public class EndField extends Field {
-
+	/**
+	*Fogadja a mezőre érkező Box-t, beállítja annak a mező referenciáját, aztán pedig kitörli a régi mezőjén lévő referenciáját (ahonnan jött, null-ra állítja). 
+	*Végezetül pedig mozdíthatatlanná teszi a mezőre érkezett Box-t. Továbbá elindítja a pontszerzési folyamatot a megfelelő munkás számára.
+	*
+	*@param b
+	*            A mezőre lépő Box referenciája.
+	*@param d
+	*            A mezőre lépő Box lépésének iránya.
+	*/
 	@Override
 	public Movable boxEnters(Box b, Direction d) {
 		MethodWriter.printOutMethod("EndField.boxEnters", b.toString() + ", " + d.toString());
 		
 		if (this.getMovable() == null) {
-
+			//Megkérdezi a felhasználotól, hogy van-e már doboz a mezőn.
 			MethodWriter.printOutQuestion("Is there a box on the field? B/X");
 
 			String answer = MethodWriter.readFromStdin();
-
+			//az adott válasz alapján a doboz létrehozása ha szükséges.
 			switch (answer) {
 			case "b":
 				Box b2 = new Box();
@@ -26,14 +44,15 @@ public class EndField extends Field {
 			}
 
 		}
-		
+		//az érkező doboz áthelyezése az új mezőre
 		b.place(this);
 		
 		Field backwardNeighbor = this.getNeighbor(d.getOpposite());
 		Movable movable = this.getMovable();
 		this.setMovable(b);
-		backwardNeighbor.remove(b);
-		
+		backwardNeighbor.remove(b); // Az érkező Worker referenciájának kitörlése arról a mezőről, ahonnan jött.
+		//ha már van doboz az EndFielden akkor visszaadja az érkező doboz referenciáját,ezzel jelezve hogy nem lehet már ide lépni
+		//mivel az a doboz már nem mozdítható
 		if (movable != null) {
 			MethodWriter.printOutRet(b.toString());
 			return b;
@@ -44,17 +63,26 @@ public class EndField extends Field {
 		MethodWriter.printOutRet("null");
 		return null;
 	}
-
+	
+	/**
+	*Fogadja a mezőre érkező Worker-t, beállítja annak a mező referenciáját, 
+	*aztán pedig kitörli a régi mezőjén lévő referenciáját (ahonnan jött, null-ra állítja).
+	*
+	* @param w
+	*            A mezőre lépő Worker referenciája.
+	* @param d
+	*            A mezőre lépő Worker lépésének iránya.
+	*/
 	@Override
 	public Movable workerEnters(Worker w, Direction d) {
 		MethodWriter.printOutMethod("EndField.workerEnters", w.toString() + ", " + d.toString());
 		
 		if (this.getMovable() == null) {
-
+			//Megkérdezi a felhasználotól, hogy van-e már doboz a mezőn.
 			MethodWriter.printOutQuestion("Is there a box on the field? B/X");
 
 			String answer = MethodWriter.readFromStdin();
-
+			//az adott válasz alapján a doboz létrehozása ha szükséges.
 			switch (answer) {
 			case "b":
 				Box b2 = new Box();
@@ -67,14 +95,15 @@ public class EndField extends Field {
 			}
 
 		}
-		
+		//az érkező doboz áthelyezése az új mezőre
 		w.place(this);
 		
 		Field backwardNeighbor = this.getNeighbor(d.getOpposite());
 		Movable movable = this.getMovable();
 		this.setMovable(w);
-		backwardNeighbor.remove(w);
-		
+		backwardNeighbor.remove(w); // Az érkező Worker referenciájának kitörlése arról a mezőről, ahonnan jött.
+		//ha már van doboz az EndFielden akkor visszaadja az érkező doboz referenciáját,ezzel jelezve hogy nem lehet már ide lépni
+		//mivel az a doboz már nem mozdítható
 		if (movable != null) {
 			MethodWriter.printOutRet(w.toString());
 			return w;
--- Workspace/Skeleton/src/skeleton/game/logic/WallField.java
+++ Workspace/Skeleton/src/skeleton/game/logic/WallField.java
@@ -2,26 +2,45 @@ package skeleton.game.logic;
 
 import skeleton.out.MethodWriter;
 
+/**
+*Olyan mező objektum, mely egy falat reprezentál. Nem lehet rajta átmenni, és ismeri a szomszédait.
+*A Field absztrakt ősosztály leszármazottja.
+* 
+* @author jgoldfisch
+*/
 public class WallField extends Field {
-
+	/**
+	* Megakadályozza, hogy egy Box rálépjen a mezőre. Ezt azzal éri el, hogy amikor rálép, akkor a paraméterben kapott objektum referenciáját adja vissza.
+	* @param b
+	*            A mezőre lépő Box referenciája.
+	* @param d
+	*            A mezőre lépő Box lépésének iránya.
+	**/
 	@Override
 	public Movable boxEnters(Box b, Direction d) {
 		MethodWriter.printOutMethod("WallField.boxEnters", b.toString() + ", " + d.toString());
-		
+		//elhelyezi az új mezőre a Boxott és törli az elözőről
 		b.place(this);
 		getNeighbor(d.getOpposite()).remove(b);
 		
 		MethodWriter.printOutRet(b.toString());
+		//a paraméterben kapott objektum referenciáját adja vissza, ezzel jelezve annak hogy vissza kell lépnie.
 		return b;
 	}
-
+	/**
+	* Megakadályozza, hogy egy Worker rálépjen a mezőre. Ezt azzal éri el, hogy amikor rálép, akkor a paraméterben kapott objektum referenciáját adja vissza.
+	* @param w
+	*            A mezőre lépő Worker referenciája.
+	* @param d
+	*            A mezőre lépő Box lépésének iránya.
+	**/
 	@Override
 	public Movable workerEnters(Worker w, Direction d) {
 		MethodWriter.printOutMethod("WallField.workerEnters", w.toString() + ", " + d.toString());
-		
+		//elhelyezi az új mezőre a Workert és törli az elözőről
 		w.place(this);
 		getNeighbor(d.getOpposite()).remove(w);
-		
+		//a paraméterben kapott objektum referenciáját adja vissza, ezzel jelezve annak hogy vissza kell lépnie.
 		MethodWriter.printOutRet(w.toString());
 		return w;
 	}
