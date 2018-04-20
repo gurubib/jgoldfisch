8ac7722cadea8f175ad106ce9be1ba3be5bb5be6
Barnabas Gurubi <gurubibarni@gmail.com>
2018-04-20 10:49:00 +0200

    Proto elkezdése - wipe out

--- Workspace/Proto/src/proto/Proto.java
+++ Workspace/Proto/src/proto/Proto.java
@@ -5,25 +5,13 @@ package proto;
 
 import java.io.IOException;
 
-import proto.logic.Map;
-
 /**
  * @author jgoldfisch
  *
  */
 public class Proto {
 
-	/**
-	 * @param args
-	 * @throws IOException 
-	 */
 	public static void main(String[] args) throws IOException {
-		
-		Map m = new Map();
-		
-		String fileName = "testmap.txt";
-		
-		m.loadMap(fileName);
 
 	}
 
